68b544a1fb5751d5dcd85f45daeb98fe7be20672
Syed Maqutadir Rehman <maqutadir.rehman@gmail.com>
2022-06-23 20:31:43 -0400

    Code to search concatenated words

--- src/trie.js
+++ src/trie.js
@@ -39,3 +39,53 @@ wordExist("Snowstrom");
 wordExist("Cat");
 wordExist("mouse");
 console.log("The trie is", Trie);
+
+function buildTrieWithLength(words) {
+  let root = {};
+  for (let word of words) {
+    let node = root;
+    for (let i of word) {
+      if (!node[i]) node[i] = {};
+      node = node[i];
+    }
+    node.end = word.length;
+  }
+  return root;
+}
+
+const TrieArray = [
+  "hello",
+  "dogmancat",
+  "cat",
+  "man",
+  "dog",
+  "catdogdog",
+  "catdogdogman",
+  "hellocat"
+];
+const TrieWithLength = buildTrieWithLength(TrieArray);
+console.log("Trie with lenght is", TrieWithLength);
+
+function seachConcatWord(
+  left,
+  original = left,
+  node = TrieWithLength,
+  count = 0,
+  memo = new Set()
+) {
+  if (memo.has(left)) return;
+  if (!left.length && count > 1) answer.push(original);
+  for (let char of left) {
+    node = node[char];
+    if (!node) return;
+    if (!node.end) continue;
+    let next = left.slice(node.end);
+    seachConcatWord(next, original, TrieWithLength, count + 1, memo);
+    memo.add(next);
+  }
+}
+
+let answer = [];
+for (let word of TrieArray) seachConcatWord(word);
+
+console.log("The concatable words present in the dictionary are", answer);
